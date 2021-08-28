d61232051c4bdf38de2ffb2d91ad12051a5db60c
Tales Almeida Santos <talessantos49@gmail.com>
2021-08-27 22:48:08 -0300

    ex07_ALP

--- Algoritmo_e_logica_pgma/ex06.c
+++ Algoritmo_e_logica_pgma/ex06.c
@@ -3,22 +3,46 @@
 #include <stdlib.h>
 
 int main (){
-	float nota1, nota2, nota3, nota4, media;
-	printf("Digite a primeira nota: ");
-	scanf("%f", &nota1);
-	printf("Digite a segunda nota: ");
-	scanf("%f", &nota2);
-	printf("Digite a terceira nota: ");
-	scanf("%f", &nota3);
-	printf("Digite a quarta nota: ");
-	scanf("%f", &nota4);
+	float numero1, numero2, numero3, numero4;
+	printf("Digite a primeira numero: ");
+	scanf("%f", &numero1);
 	
-	media = (nota1+nota2+nota3+nota4)/4;
-		if (media>=5){
-			printf("Sua media foi %.2f, Parabens voce foi aprovado.", media);
-		}
-		else{
-			printf("Sua media foi %.2f, Que pena voce foi reprovado.", media);
-		}
-		 
+	printf("Digite o segundo numero: ");
+	scanf("%f", &numero2);
+	
+	while (numero1>=numero2){
+		printf("Obs: A ordem dos numeros tem que ser crescentes. \nDigite novamente o segundo numero:  ");
+		scanf("%f", &numero2);
+		if (numero1<numero2){
+			break;
+		}	
 	}
+	
+	printf("Digite o terceiro numero: ");
+	scanf("%f", &numero3);
+
+		while (numero2>=numero3){
+		printf("Obs: A ordem dos numeros tem que ser crescentes. \nDigite novamente o terceiro numero: ");
+		scanf("%f", &numero3);
+		if (numero2<numero3){
+			break;
+		}	
+	}
+
+	printf("Digite o quarto numero: ");
+	scanf("%f", &numero4);
+	
+	if ((numero4>=numero1) && (numero4<numero2) && (numero4<numero3)){
+		printf("Os valores em ordem decrescente : %.2f, %.2f, %.2f e %.2f", numero1, numero4, numero2, numero3);
+	}
+	if ((numero4>=numero2) && (numero4<numero3)){
+		printf("Os valores em ordem decrescente : %.2f, %.2f, %.2f e %.2f", numero1, numero2, numero4, numero3);
+	}
+	if (numero4<numero1){
+		printf("Os valores em ordem decrescente : %.2f, %.2f, %.2f e %.2f", numero4, numero1, numero2, numero3);
+	}
+	if (numero4>=numero3){
+		printf("Os valores em ordem decrescente : %.2f, %.2f, %.2f e %.2f", numero1, numero2, numero3, numero4);
+	}
+
+}
