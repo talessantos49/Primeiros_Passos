4b8151d0e3367848c2284f60cdf51a0684d41bb9
talessantos49 <talessantos49@gmail.com>
2022-08-31 00:15:54 -0300

    Resume First Version: 0.0.1

--- Fatec/Segundo_Semestre/Tecnicas_de_Programacao/src/TesteVendedor.java
+++
@@ -1,7 +0,0 @@
-public class TesteVendedor {
-    public static void main(String [] args){
-        Vendedor v1 = new Vendedor(1500f, 500f, "Pedro", 0);
-        v1.calcularSalario();
-        v1.imprimirDados();
-    }
-}
